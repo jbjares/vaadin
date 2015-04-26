53746e783cb3957b021049b861918bc8701e98da
jbjares <joao.jares@insight-centre.org>
2015-04-26 07:45:59 +0100

    first commit

---
+++ src/main/java/com/vaadin/demo/dashboard/view/dashboard/DashboardAddDatasource.java
@@ -0,0 +1,59 @@
+package com.vaadin.demo.dashboard.view.dashboard;
+
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.BrowserFrame;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.themes.ValoTheme;
+
+/**
+ * Simple name editor Window.
+ */
+@SuppressWarnings("serial")
+public class DashboardAddDatasource extends Window {
+
+    private final DashboardAddDatasourceListener listener;
+
+    public DashboardAddDatasource(final DashboardAddDatasourceListener listener,
+            final String currentName) {
+        this.listener = listener;
+        setCaption("Add Datasource");
+        setModal(true);
+        setClosable(true);
+        setResizable(false);
+        setWidth(600.0f, Unit.PIXELS);
+        setHeight(600.0f, Unit.PIXELS);
+        addStyleName("edit-dashboard");
+
+        setContent(buildContent(currentName));
+    }
+
+    private Component buildContent(final String currentName) {
+        VerticalLayout result = new VerticalLayout();
+        result.setMargin(true);
+        result.setSpacing(true);
+
+		BrowserFrame shinyAppFileUpload = new BrowserFrame("",
+		new ExternalResource("http://127.0.0.1:7161/?cmd=fileUpload"));
+		shinyAppFileUpload.addStyleName(ValoTheme.LABEL_H1);
+		shinyAppFileUpload.addStyleName(ValoTheme.LABEL_NO_MARGIN);
+		shinyAppFileUpload.setHeight("400");
+		shinyAppFileUpload.setWidth("400");
+		result.addComponent(shinyAppFileUpload);
+
+        return result;
+    }
+
+
+    public interface DashboardAddDatasourceListener {
+        void dashboardNameEdited(String name);
+    }
+}
